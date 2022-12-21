ef7cc2277a1f0bd2847681d49d985433906cd757
Vadim Babajanyan <babadzhanyan@corp.mail.ru>
2022-12-21 20:22:23 +0300

    no message

---
+++ users_reset.py
@@ -0,0 +1,39 @@
+import os
+import sys
+import csv
+import json
+import freeipa
+
+settings = {}
+
+settings['host'] = os.environ.get('FREEIPA_HOST') or ""
+settings['login'] = os.environ.get('FREEIPA_LOGIN') or ""
+settings['password'] = os.environ.get('FREEIPA_PASSWORD') or ""
+settings['username'] = ""
+settings['group'] = ""
+settings['check'] = False
+settings['reset'] = False
+settings['otp'] = False
+
+for k, v in enumerate(sys.argv):
+    if v:
+        t = v.split("=")
+        if len(t) == 2 and t[0] in settings and t[1]:
+            settings[t[0]] = t[1]
+
+with open('users_reset.csv', newline="\n") as fp:
+    reader = csv.reader(fp, delimiter=";", quotechar='"')
+    for row in reader:
+        if len(row) != 2:
+            continue
+        ipa = freeipa.Freeipa(host=settings['host'],
+                              login=settings['login'],
+                              password=settings['password'],
+                              username=row[0],
+                              group=settings['group'],
+                              check=settings['check'],
+                              reset=settings['reset'],
+                              otp=settings['otp']
+                              )
+        results = ipa.run_actions()
+        print(results)
---
+++ users_reset.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+set -a
+source .env
+set +a
+
+usage() { 
+    echo "Usage: $0
+    -H <host>     : freeipa host with https://
+    -l <login>    : freeipa admin login
+    -p <password> : freeipa admin password
+    -u <username> : freeipa username
+    -c            : check login
+    -g <group>    : add to group
+    -r            : reset password 
+    -o            : reset otp";
+    exit 1; 
+}
+
+while getopts H:l:p:g:u:croh flag
+do
+    case "${flag}" in
+        H) host=${OPTARG};;
+        l) login=${OPTARG};;
+        p) password=${OPTARG};;
+        g) group=${OPTARG};;
+        u) username=${OPTARG};;
+        c) check=1;;
+        r) reset=1;;
+        o) otp=1;;
+        h) usage;;
+        *) usage;;
+    esac
+done
+
+python3 users_reset.py host=$host login=$login password=$password username=$username group=$group reset=$reset otp=$otp check=$check
