d57d9b8b7fde75a76f1c4d29481b10e43e16dd04
Vadim Babajanyan <babadzhanyan@corp.mail.ru>
2022-12-21 10:52:11 +0300

    no message

--- freeipa.py
+++ freeipa.py
@@ -44,7 +44,7 @@ class App:
     def collect_result(self, key: str, result: object, error: str = None, exit: bool = False):
         self.result[key] = result
         if error:
-            if not self.result[key] or type(self.result[key]) != object:
+            if not self.result[key]:
                 self.result[key] = {}
             self.result[key]['error'] = error
         if exit:
